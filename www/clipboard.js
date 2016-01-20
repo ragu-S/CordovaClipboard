e20a388774723d8238d8e61cdf46c16deec49287
Ragu Sivanandha <ragu.sivanandha@gmail.com>
2016-01-20 16:10:49 -0500

    Update clipboard.js

--- www/clipboard.js
+++ www/clipboard.js
@@ -33,13 +33,14 @@
 // Register the plugin
 //var clipboard = new Clipboard();
 //module.exports = clipboard;
+var exec = require("cordova/exec");
 
 module.exports = {
 	copy: function (text, onSuccess, onFail) {
     		if (typeof text === "undefined" || text === null) text = "";
-		cordova.exec(onSuccess, onFail, "Clipboard", "copy", [text]);
+		exec(onSuccess, onFail, "Clipboard", "copy", [text]);
 	},
 	paste: function (onSuccess, onFail) {
-		cordova.exec(onSuccess, onFail, "Clipboard", "paste", []);
+		exec(onSuccess, onFail, "Clipboard", "paste", []);
 	}
 };
